b5d1245b7c891a33d7f03c992e7d286698961490
Pablo Jurado <pjurado85@gmail.com>
2017-04-10 21:20:05 -0500

    create Ajax call and callback functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,21 @@
-console.log('hi')
+var $ = window.jQuery
+
+$('#loginBtn').click(loginHandler)
+
+function loginHandler (e) {
+  e.preventDefault()
+  // var $login = $('#loginInput').val()
+  // var $pass = $('#passwordInput').val()
+  // console.log($login, $pass)
+  var url = 'http://127.0.0.1:7979/api/login'
+  var data = { username: 'qauser3', password: 'luv2manage5tate' }
+  $.post(url, data).done(response).fail(responseFail)
+}
+
+function response (data) {
+  console.log('done!', data)
+}
+
+function responseFail (err) {
+  console.log('error :(', err)
+}
