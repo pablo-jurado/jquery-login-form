86b5f364bcf81604a3c412d2b478edb7c2c38651
Pablo Jurado <pjurado85@gmail.com>
2017-04-12 12:29:15 -0500

    add spinner animation for waiting state

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,41 @@
 var $ = window.jQuery
+var btn = $('#loginBtn')
+var spinner = $('.spinner')
 
-$('#loginBtn').click(loginHandler)
-
-function loginHandler (e) {
-  e.preventDefault()
-  // var $login = $('#loginInput').val()
-  // var $pass = $('#passwordInput').val()
-  // console.log($login, $pass)
-  var url = 'http://127.0.0.1:7979/api/login'
-  var data = { username: 'qauser3', password: 'luv2manage5tate' }
-  $.post(url, data).done(response).fail(responseFail)
-}
+spinner.hide()
 
 function response (data) {
+  spinner.hide()
+  btn.attr('disabled', false)
   console.log('done!', data)
 }
 
 function responseFail (err) {
+  spinner.hide()
+  btn.attr('disabled', false)
   console.log('error :(', err)
 }
+
+function waitingForResponse () {
+  btn.attr('disabled', true)
+  spinner.show()
+}
+
+function loginHandler (e) {
+  e.preventDefault()
+  waitingForResponse()
+  var $login = $('#loginInput').val()
+  var $pass = $('#passwordInput').val()
+  var url = 'http://localhost:7979/api/login'
+  var data = { username: $login, password: $pass }
+  $.post(url, data).done(response).fail(responseFail)
+}
+
+btn.click(loginHandler)
+
+//
+// user names for testing
+//
+console.log('testuser1 : ilovebananas')
+console.log('testadmin5 : always_name_your_functions')
+console.log('qauser3 : luv2manage5tate')
